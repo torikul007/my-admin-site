71f27372e3811b34832ef6be420ff8f45c69dcb3
Md . Torikul islam <111352451+torikul007@users.noreply.github.com>
2026-04-17 15:38:10 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,28 +1,35 @@
 const repo = "YOUR_USERNAME/YOUR_REPO";
 const token = "YOUR_GITHUB_TOKEN";
 
+// protect admin page
+if (localStorage.getItem("role") !== "admin") {
+  alert("Access denied!");
+  window.location.href = "index.html";
+}
+
 async function loadUsers() {
   const res = await fetch(`https://api.github.com/repos/${repo}/contents/data/users.json`, {
-    headers: {
-      Authorization: `token ${token}`
-    }
+    headers: { Authorization: `token ${token}` }
   });
 
-  const data = await res.json();
-  const content = atob(data.content);
-  const json = JSON.parse(content);
+  const file = await res.json();
+  const data = JSON.parse(atob(file.content));
 
   const container = document.getElementById("users");
   container.innerHTML = "";
 
-  json.users.forEach(u => {
+  data.users.forEach(u => {
     const div = document.createElement("div");
     div.className = "card";
+
     div.innerHTML = `
       <b>Email:</b> ${u.email}<br>
       <b>Password:</b> ${u.password}<br>
       <b>Time:</b> ${u.time}
     `;
+
     container.appendChild(div);
   });
 }
+
+loadUsers();
